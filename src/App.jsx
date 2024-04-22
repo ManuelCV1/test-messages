22d5be5a107cebee2e2d29b5b034812c61ef7f12
ManuelCV1 <manuelcabrera.cv@gmail.com>
2024-04-22 13:07:06 -0400

    finish

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import MessagesInterface from "./MessagesInterface";
+
+function App() {
+  return (
+    <>
+      <MessagesInterface />
+    </>
+  );
+}
+
+export default App;
---
+++ src/MessagesInterface.tsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./MessagesInterface.css";
+
+const MessagesInterface = () => {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "http://www.test.readychatai.com/data"
+        );
+        setMessages(response.data);
+        console.log(response);
+      } catch (error) {
+        console.error("Hubo un error al obtener los datos:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="messages-container">
+      {messages.map((message: any) => (
+        <div
+          key={message.id}
+          className={`message ${message.bot_sender ? "bot" : "user"}`}
+        >
+          <div className="message-info">
+            <span className="sender-name">{message.sender_name}</span>
+            <span className="message-date">{message.message_date}</span>
+          </div>
+          <div className="message-text">{message.message_text}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default MessagesInterface;
